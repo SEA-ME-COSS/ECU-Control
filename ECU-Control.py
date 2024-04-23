b09dfd2cb14ff82dc58ed1a510f074451d7dd436
lalywr2000 <12190450@inha.edu>
2024-04-23 16:06:55 +0200

    CAN Test

--- ECU-Control.py
+++ ECU-Control.py
@@ -4,66 +4,71 @@ import can
 from piracer.vehicles import PiRacerStandard
 
 
-# current_steering_data = multiprocessing.Value('f', 0.0)
-# current_throttle_data = multiprocessing.Value('f', 0.0)
-# steering_lock = multiprocessing.Lock()
-# throttle_lock = multiprocessing.Lock()
+current_steering_data = multiprocessing.Value('f', 0.0)
+current_throttle_data = multiprocessing.Value('f', 0.0)
+steering_lock = multiprocessing.Lock()
+throttle_lock = multiprocessing.Lock()
 
 bus = can.interface.Bus(channel='can0', bustype='socketcan')
 
+piracer = PiRacerStandard()
+piracer.set_steering_percent(0.0)
+piracer.set_throttle_percent(0.0)
+time.sleep(0.5)
+
+# -------------------------------------------------- READY SIGN
+piracer.set_steering_percent(0.9)
+time.sleep(1)
+piracer.set_steering_percent(-0.9)
+time.sleep(1)
+piracer.set_steering_percent(0.0)
+# -------------------------------------------------- READY SIGN
+
 
 def receive_can_data():
+    global current_steering_data
+    global current_throttle_data
+
     while True:
         message = bus.recv()
-        print("Received message - ID:", message.arbitration_id, "Data:", message.data)
+        data = message.data[1] + message.data[2] * 0.01
+        if message.data[0] == 1:
+            data *= -1
+
+        if message.arbitration_id == 0:
+            with steering_lock:
+                current_steering_data.value = data
+        if message.arbitration_id == 1:
+            with throttle_lock:
+                current_throttle_data.value = data
 
 
-# 값을 저장하는 함수
-# def store_value():
-#     global shared_variable
-#     while True:
-#         with lock:
-#             shared_variable.value += 1
-#         time.sleep(1)  # 1초마다 값을 증가시킴
+def print_value():
+    global current_steering_data
+    global current_throttle_data
+
+    while True:
+        with steering_lock:
+            print("steering:", current_steering_data.value)
+        with throttle_lock:
+            print("throttle:", current_throttle_data.value)
+        time.sleep(3)
 
 
-# def print_value():
-#     global shared_variable
-#     while True:
-#         with lock:
-#             print("Current value:", shared_variable.value)
-#         time.sleep(3)  # 3초마다 현재 값을 출력
-
-# 프로세스 생성
 store_process = multiprocessing.Process(target=receive_can_data)
-# print_process = multiprocessing.Process(target=print_value)
+print_process = multiprocessing.Process(target=print_value)
 
-# 프로세스 시작
 store_process.start()
-# print_process.start()
+print_process.start()
 
-# 메인 프로세스 종료 대기
 store_process.join()
-# print_process.join()
+print_process.join()
 
 
 # ==========================
 
 
-# piracer = PiRacerStandard()
-# piracer.set_steering_percent(current_steering0.0)
-# piracer.set_throttle_percent(0.0)
-# time.sleep(0.5)
 
-# # Ready Sign
-# piracer.set_steering_percent(0.9)
-# time.sleep(1)
-# piracer.set_steering_percent(-0.9)
-# time.sleep(1)
-# piracer.set_steering_percent(0.0)
-
-# current_steering = 0.0
-# current_throttle = 0.0
 
 
 # def process_can_message(msg):
