77b89f615fa287c15db3ac1694db01a89889a3df
lalywr2000 <12190450@inha.edu>
2024-04-23 19:31:21 +0200

    Add sub node

--- ecu_control_ws/src/ecu_control/setup.py
+++ ecu_control_ws/src/ecu_control/setup.py
@@ -21,6 +21,7 @@ setup(
     entry_points={
         'console_scripts': [
             'publisher = ecu_control.publisher:main',
+            'subscriber = ecu_control.subscriber:main',
         ],
     },
 )
