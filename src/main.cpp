1d1cb6f1efd43267d725a4eb184f6bfc746195ee
lalywr2000 <12190450@inha.edu>
2024-04-26 14:43:28 +0200

    Print id

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 
 int main ()
 {
-    pthread_t readCANThread, controlThread;
+    pthread_t readCANThread;  //, controlThread;
     
     pthread_create(&readCANThread, NULL, ReadCANThread, NULL);
-    pthread_create(&controlThread, NULL, ControlThread, NULL);
+    // pthread_create(&controlThread, NULL, ControlThread, NULL);
 
     pthread_join(readCANThread, NULL);
     // pthread_join(controlThread, NULL);
