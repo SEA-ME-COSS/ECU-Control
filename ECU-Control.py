96ae63f7f44a9d74348716ed015be1d8303df12d
lalywr2000 <12190450@inha.edu>
2024-04-30 15:33:53 +0200

    Shutdown can

--- ECU-Control.py
+++ ECU-Control.py
@@ -3,17 +3,20 @@ from piracer.vehicles import PiRacerStandard
 
 
 bus = can.interface.Bus(channel='can0', bustype='socketcan')
-
 piracer = PiRacerStandard()
 
-while True:
-    message = bus.recv()
+try:
+    while True:
+        message = bus.recv()
 
-    data = message.data[1] + message.data[2] * 0.01
-    if message.data[0] == 1:
-        data *= -1
+        data = message.data[1] + message.data[2] * 0.01
+        if message.data[0] == 1:
+            data *= -1
 
-    if message.arbitration_id == 0:
-        piracer.set_steering_percent(data)
-    if message.arbitration_id == 1:
-        piracer.set_throttle_percent(data)
+        if message.arbitration_id == 0:
+            piracer.set_steering_percent(data)
+        if message.arbitration_id == 1:
+            piracer.set_throttle_percent(data)
+
+except KeyboardInterrupt:
+    bus.shutdown()
